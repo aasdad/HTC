d679842ebc1c9c4856e83fbc261f364e292e028a
ZKQ <2437266454@qq.com>
2017-02-10 21:46:28 +0800

    第一天晚上提交

--- js/index.js
+++ js/index.js
@@ -1,4 +1,38 @@
 $(function(){
+	//轮播图
 	var aa=new shuffing(".bannerbox",1329,532,15,15,5,40,40);
+	//footer
+	$(".footer-middle").toggle(function(){
+		 $(".footer-bottom").css("display","block")
+		 $(".footer-main").animate({top:"-412px"},1000)
+		
+	},function(){
+		$(".footer-main").animate({top:"0px"},1000,function(){
+			 $(".footer-bottom").css("display","none")
+		})
+	})
+	
+     //video	
+	$(".video").hover(function(){
+		$(".shadow").css("display","none");
+	},function(){
+		$(".shadow").css("display","block");
+	})
+	
+	$(".welcome").mouseover(function(){
+		$(this).addClass("active")
+	})
+	$(".welcome").mouseout(function(){
+		$(this).removeClass("active")
+	})
+	
+	//buybutton
+	$(".buybutton").hover(function(){
+		$(this).addClass("active")
+	},function(){
+		$(this).removeClass("active")
+	})
+	
+	
 	
 })
