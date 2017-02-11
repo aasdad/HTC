3843449edd4bb81bb21e6ec6ebb5dab8131691a6
ZKQ <2437266454@qq.com>
2017-02-11 17:24:56 +0800

    第二天中午

---
+++ js/shop.js
@@ -0,0 +1,12 @@
+$(function(){
+	
+ /* nav	*/
+	$(".nav ul li a").hover(function(){
+	  $(this).css("background","#A5D04D")
+	},function(){
+	  $(this).css("background","none")
+	})
+	
+  //轮播图
+	var aa=new shuffing(".bannerbox",1349,532,15,15,5,40,40);	
+})
